42246d6b39c3cf5029b04b91651c4a4cc2f9e5f0
Anthony Mercado <antho@Anthonys-Mini.attlocal.net>
2022-09-09 18:48:19 -0500

    :tada: Replaced Nuxt with Next

--- pages/social.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1 class="text-center">SOCIALS</h1>
-</template>
-
-<script>
-export default {
-  name: 'SocialPage',
-}
-</script>
--- pages/tour.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>TOUR</h1>
-</template>
-
-<script>
-export default {
-  name: 'TourPage',
-}
-</script>
