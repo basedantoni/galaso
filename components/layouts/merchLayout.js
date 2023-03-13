239a774036617e989acaebe9e0f86874a36e95b5
Anthony Mercado <antho@Anthonys-Mini.attlocal.net>
2023-03-13 00:30:07 -0500

    :lipstick: Fix merch layout styling

--- components/layouts/merchLayout.js
+++ components/layouts/merchLayout.js
@@ -5,14 +5,14 @@ export default function MerchLayout({ children }) {
     <>
       <main>
         <Link href="/tour">
-          <a className="text-6xl sm:text-base top-2 left-1 p-16 sm:top-5 sm:left-4 sm:p-7 fixed font-bold text-white">TOUR</a>
+          <a className="text-base top-2 left-1 p-4 sm:top-5 sm:left-4 sm:p-7 fixed font-bold text-white">TOUR</a>
         </Link>
         <Link href="/music">
-          <a className="text-6xl sm:text-base top-2 right-1 p-16 sm:top-5 sm:right-4 sm:p-7 fixed font-bold text-white">MUSIC</a>
+          <a className="text-base top-2 right-1 p-4 sm:top-5 sm:right-4 sm:p-7 fixed font-bold text-white">MUSIC</a>
         </Link>
-        <a className="text-6xl sm:text-base bottom-2 left-1 sm:bottom-5 sm:left-4 p-16 sm:p-7 fixed font-bold text-white" href="https://www.instagram.com/galaso__/" target="_blank" rel="noreferrer">IG</a>
+        <a className="text-base bottom-2 left-1 sm:bottom-5 sm:left-4 p-4 sm:p-7 fixed font-bold text-white" href="https://www.instagram.com/galaso__/" target="_blank" rel="noreferrer">IG</a>
         <Link href="/merch">
-          <a className="text-6xl sm:text-base bottom-2 right-1 sm:bottom-5 sm:right-4 p-16 sm:p-7 fixed font-bold text-white">MERCH</a>
+          <a className="text-base bottom-2 right-1 sm:bottom-5 sm:right-4 p-4 sm:p-7 fixed font-bold text-white">MERCH</a>
         </Link>
         <div>
           {children}
