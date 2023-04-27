206095bbd972137efce77241fc95ec019ad5a7f7
Anthony Mercado <antho@Anthonys-Mini.attlocal.net>
2023-04-26 21:47:08 -0500

    GAL-3 click and hover work

--- components/Shirt.js
+++ components/Shirt.js
@@ -11,11 +11,76 @@ import SceneInit from '../lib/sceneInit';
 
 function App() {
   const isMedium = useMediaQuery('(min-width: 768px)');
+
   
   useEffect(() => {
+    const modelMeshes = [];
+    let boundingBox;
     const test = new SceneInit('myThreeJsCanvas');
     test.initialize();
     test.animate();
+
+    // Update the onMouseMove function
+    function onMouseMove(event) {
+      // Normalize mouse coordinates (-1 to +1)
+      test.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      test.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      // Update the raycaster with the current camera and mouse positions
+      test.raycaster.setFromCamera(test.mouse, test.camera);
+
+      // Test for intersections with objects in the scene
+      const intersects = test.raycaster.intersectObjects(modelMeshes, true);
+
+      if (intersects.length > 0) {
+        // The first intersected object is the closest one to the camera
+        const hoveredObject = intersects[0].object;
+
+        // Check if the hovered object is part of the GLTF model
+        if (loadedModel && loadedModel.scene.children.includes(hoveredObject)) {
+          // Change the cursor style to a pointer
+          document.body.style.cursor = 'pointer';
+          // Disable model rotation
+          test.modelRotationEnabled = false;
+        } else {
+          // Revert the cursor style back to the default
+          document.body.style.cursor = 'default';
+          // Enable model rotation
+          test.modelRotationEnabled = true;
+        }
+      } else {
+        // Revert the cursor style back to the default
+        document.body.style.cursor = 'default';
+        // Enable model rotation
+        test.modelRotationEnabled = true;
+      }
+    }   
+
+    function onClick(event) {
+      event.preventDefault();
+    
+      // Calculate normalized device coordinates (NDC) of mouse click
+      const mouse = new THREE.Vector2(
+        (event.clientX / window.innerWidth) * 2 - 1,
+        -(event.clientY / window.innerHeight) * 2 + 1
+      );
+    
+      // Set up a raycaster using the camera and mouse coordinates
+      const raycaster = new THREE.Raycaster();
+      raycaster.setFromCamera(mouse, test.camera);
+    
+      // Test for intersections with objects in the scene
+      const intersects = raycaster.intersectObjects(modelMeshes, true);
+    
+      if (intersects.length > 0) {
+        console.log('Clicked on GLTF model');
+        const externalUrl = 'https://jacobsfirststore-1319-2.myshopify.com/';
+        window.open(externalUrl, '_blank', 'noopener,noreferrer');
+      }
+    }
+
+    window.addEventListener('mousemove', onMouseMove, false);
+    window.addEventListener('click', onClick, false);
 
     const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
     const boxMaterial = new THREE.MeshNormalMaterial();
@@ -43,15 +108,18 @@ function App() {
       // test.scene.add(textMesh);
 
       // COMING SOON TEXT
-      const comingSoonTextGeo = new TextGeometry('COMING SOON', {
+      const comingSoonTextGeo = new TextGeometry(
+      `STARGAZING TEE 
+             
+    IN THE DARK`, {
         height: isMedium ? 3.5 : 2.5,
-        size: isMedium ? 9 : 2.5,
+        size: isMedium ? 7.5 : 2,
         font: boskFont,
       });
       const comingSoonTextMaterial = new THREE.MeshNormalMaterial();
       const comingSoonTextMesh = new THREE.Mesh(comingSoonTextGeo, comingSoonTextMaterial);
-      comingSoonTextMesh.position.x = isMedium ? -32 : -9;
-      comingSoonTextMesh.position.y = -1;
+      comingSoonTextMesh.position.x = isMedium ? -34 : -9;
+      comingSoonTextMesh.position.y = 3;
       comingSoonTextMesh.position.z = isMedium ? -12 : -8;
       test.scene.add(comingSoonTextMesh);
     });
@@ -59,7 +127,24 @@ function App() {
     let loadedModel;
     const glftLoader = new GLTFLoader();
     glftLoader.load('please.gltf', (gltfScene) => {
+      // After loading the GLTF model, assign it to the test instance
+      test.loadedModel = gltfScene;
       loadedModel = gltfScene;
+
+      gltfScene.scene.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          modelMeshes.push(child);
+        }
+      });
+      
+      const box3 = new THREE.Box3().setFromObject(gltfScene.scene);
+      const size = new THREE.Vector3();
+      box3.getSize(size);
+      const geometry = new THREE.BoxGeometry(size.x * 2, size.y * 10, size.z);
+      const material = new THREE.MeshBasicMaterial({ visible: false });
+      boundingBox = new THREE.Mesh(geometry, material);
+      boundingBox.position.y = isMedium ? -15 : -10;
+      boundingBox.position.x = isMedium ? -1 : 0.5;
 
       gltfScene.scene.rotation.y = Math.PI / 5;
       gltfScene.scene.position.y = isMedium ? -15 : -10;
@@ -68,23 +153,12 @@ function App() {
       test.scene.add(gltfScene.scene);
     });
 
-    const animate = () => {
-      if (loadedModel) {
-        // loadedModel.scene.rotation.x += 0.01;
-        loadedModel.scene.rotation.y += 0.01;
-        // loadedModel.scene.rotation.z += 0.01;
-      }
-      requestAnimationFrame(animate);
+
+    // Add a cleanup function to remove event listeners when the component is unmounted
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove, false);
+      window.removeEventListener('click', onClick, false);
     };
-    animate();
-
-    //SHOPIFY CLICK UNCOMMENT FOR LAUNCH
-    // document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-
-    function onDocumentMouseDown( event ) 
-    {
-        // Add redirect to shopify store
-    }
   }, [isMedium]);
 
   return (
