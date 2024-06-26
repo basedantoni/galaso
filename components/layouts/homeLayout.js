ee8fbad68376e4bf513162248b89dfbcee011c8b
Anthony Mercado <antho@Kendyls-MacBook-Air.local>
2024-06-25 20:47:08 -0500

    contrast

--- components/layouts/homeLayout.js
+++ components/layouts/homeLayout.js
@@ -5,14 +5,14 @@ export default function HomeLayout({ children }) {
     <>
       <main className='overflow-hidden h-screen w-full bg-cover bg-[top_0%_left_52%] sm:bg-center bg-landing-background-out'>
         <div className="relative flex justify-center sm:justify-start gap-4 sm:gap-8 py-5 px-6 sm:px-12 text-xl font-semibold font-allrounder text-white">
-          <Link className="z-10 top-2 left-1 p-4 sm:top-5 sm:left-4 sm:p-7 fixed font-semibold font-allrounder" href="/tour">
+          <Link className="[text-shadow:_0_-4px_16px_rgb(0_0_0_/_100%)] z-10 top-2 left-1 p-4 sm:top-5 sm:left-4 sm:p-7 fixed font-semibold font-allrounder" href="/tour">
             TOUR
           </Link>
-          <Link className="z-10 top-2 right-1 p-4 sm:top-5 sm:right-4 sm:p-7 fixed font-semibold font-allrounder" href="/music">
+          <Link className="[text-shadow:_0_-4px_16px_rgb(0_0_0_/_100%)] z-10 top-2 right-1 p-4 sm:top-5 sm:right-4 sm:p-7 fixed font-semibold font-allrounder" href="/music">
             MUSIC
           </Link>
-          <a className="z-10 bottom-2 left-1 sm:bottom-5 sm:left-4 p-4 sm:p-7 fixed font-semibold font-allrounder" href="https://www.instagram.com/galaso__/" target="_blank" rel="noreferrer">IG</a>
-          <a className="z-10 bottom-2 right-1 sm:bottom-5 sm:right-4 p-4 sm:p-7 fixed font-semibold font-allrounder" href="https://galasostore.myshopify.com/" target="_blank" rel="noreferrer">
+          <a className="[text-shadow:_0_-4px_16px_rgb(0_0_0_/_100%)] z-10 bottom-2 left-1 sm:bottom-5 sm:left-4 p-4 sm:p-7 fixed font-semibold font-allrounder" href="https://www.instagram.com/galaso__/" target="_blank" rel="noreferrer">IG</a>
+          <a className="[text-shadow:_0_-4px_16px_rgb(0_0_0_/_100%)] z-10 bottom-2 right-1 sm:bottom-5 sm:right-4 p-4 sm:p-7 fixed font-semibold font-allrounder" href="https://galasostore.myshopify.com/" target="_blank" rel="noreferrer">
             MERCH
           </a>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
         <main className='relative flex justify-center items-center h-screen'>
           <a href="https://too.fm/n2n8ne2" target="_blank" rel="noreferrer">
-            <p className='[text-shadow:_0_-4px_16px_rgb(100_100_100_/_100%)] cursor-pointer absolute tracking-tighter text-white top-1/3 right-16 sm:left-[15%] flex flex-col items-end sm:items-start font-allrounder font-bold text-2xl sm:text-2xl'>
+            <p className='[text-shadow:_0_-4px_16px_rgb(0_0_0_/_100%)] cursor-pointer absolute tracking-tighter text-white top-1/3 right-16 sm:left-[15%] flex flex-col items-end sm:items-start font-allrounder font-bold text-2xl sm:text-2xl'>
               <span className='sm:mr-24 sm:leading-[90%]'>click to</span>
               <span className='sm:text-5xl sm:leading-[90%]'>listen to</span>
               <span className='text-4xl sm:text-7xl sm:leading-[90%]'>Talk</span>
